fa935059e1bf314e376555ffc10b4e7d2bdc898e
uedake <uedakensuke722@gmail.com>
2025-09-04 13:56:35 +0900

    update label_stat func. Now can read more flexible folder format

--- util/label_stat.py
+++ util/label_stat.py
@@ -4,21 +4,28 @@ import csv
 import pandas as pd
 import os
 
+
 class LabelCounter:
     def __init__(self, root_dir: str):
         self.root_dir = root_dir
         self.label_list = self.read_label_def(f"{root_dir}/labels.txt")
 
-    def _count_label(self, dir_path):
+    def _count_label(self, label_dir: str, file_name_list):
         label_count = {}
-        for path in glob(f"{dir_path}/*.txt"):
-            with open(path) as f:
-                reader = csv.reader(f, delimiter=" ")
-                for line in reader:
-                    label = self.label_list[int(line[0])]
-                    if label not in label_count:
-                        label_count[label] = 0
-                    label_count[label] += 1
+        for file_name in file_name_list:
+            paths = glob(f"{label_dir}/**/{file_name}")
+            if len(paths) == 0:
+                print(f"ignore: {file_name} not found")
+            elif len(paths) >= 2:
+                print(f"ignore: {file_name} found more than 1")
+            else:
+                with open(paths[0]) as f:
+                    reader = csv.reader(f, delimiter=" ")
+                    for line in reader:
+                        label = self.label_list[int(line[0])]
+                        if label not in label_count:
+                            label_count[label] = 0
+                        label_count[label] += 1
         return label_count
 
     @staticmethod
@@ -27,11 +34,27 @@ class LabelCounter:
             return [line.strip() for line in f.readlines()]
 
     def print_labels_count(self):
-        subdirs = ["train", "val"]
-        data = [
-            self._count_label(f"{self.root_dir}/labels/{subdir}") for subdir in subdirs
-        ]
-        df = pd.DataFrame(data, index=subdirs)
+        image_file_names = {}
+        for image_path in glob(f"{self.root_dir}/images/**/*.*", recursive=True):
+            group = os.path.realpath(
+                os.path.dirname(image_path), start=f"{self.root_dir}/images"
+            )
+            if group not in image_file_names:
+                image_file_names[group] = []
+            image_file_names[group].append(os.path.basename(image_path))
+
+        data = {
+            subdir: self._count_label(
+                f"{self.root_dir}/labels",
+                [
+                    f"{os.path.splitext(file_name)[0]}.txt"
+                    for file_name in file_name_list
+                ],
+            )
+            for subdir, file_name_list in image_file_names.items()
+        }
+        df = pd.DataFrame(data.values(), index=data.keys())
         print(df.T.sort_index())
+
 
 # %%
