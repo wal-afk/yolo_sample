d2953805af71ddcccf865a40e1d4ef8283633711
uedake <uedakensuke722@gmail.com>
2025-09-05 15:53:11 +0900

    remove print

--- util/show_util.py
+++ util/show_util.py
@@ -41,7 +41,6 @@ def create_yolo_GT_image(image_path, label_path, class_names):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     height, width, _ = image.shape
-    print(type(image), image.shape, image.dtype)
     with open(label_path, "r") as f:
         lines = f.readlines()
 
